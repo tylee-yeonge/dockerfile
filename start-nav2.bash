095a7ccc2ae544950eb9b998393da6d95c741661
이태영 <tylee@thirarobotics.com>
2023-08-02 21:08:36 +0900

    update start-nav2.bash

--- start-nav2.bash
+++ start-nav2.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /humble-ws
 source /opt/ros/humble/setup.bash && source install/setup.bash
-ros2 launch nav2_bringup bringup_launch.py use_sim_time:=True autostart:=False map:=$HOME/map.yaml
+ros2 launch nav2_bringup bringup_launch.py use_sim_time:=True autostart:=False map:=/map-data/map.yaml
