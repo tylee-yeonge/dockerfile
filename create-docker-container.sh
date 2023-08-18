7b16bfc0ae08a5b26beb4b11fcaf7900f83f682b
이태영 <tylee@thirarobotics.com>
2023-08-18 19:19:43 +0900

    for m2 mac

--- create-docker-container.sh
+++ create-docker-container.sh
@@ -2,9 +2,11 @@ set -e
 IMAGE=$1
 CONTAINER=$2
 docker create -it \
-  --env="DISPLAY" \
+  --env="DISPLAY=host.docker.internal:0" \
   --net="host" \
   --name "${CONTAINER}"\
+  --platform linux/amd64 \
   --privileged \
   -v "/etc/localtime:/etc/localtime:ro" -e TZ=Asia/Seoul \
+  -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   "${IMAGE}"
